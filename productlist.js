a4b42f0d5fcb505de5f8f1f2d3cba52b072e40d5
Edith Obdrup <edob1000@stud.ek.dk>
2026-09-22 14:07:57 +0200

    json hentet

--- productlist.js
+++ productlist.js
@@ -1,40 +1,51 @@
-const tops = [
-  {
-    brand: "Pull and bear",
-    sizes: ["s", "m", "l", "xl"],
-    color: "pink",
-    price: "350kr.",
-  },
-  {
-    brand: "Next",
-    sizes: ["s", "m", "l"],
-    color: "black",
-    price: "325kr",
-  },
-  {
-    brand: "Pull and bear",
-    sizes: ["m", "l", "xl"],
-    color: "brown",
-    price: "400kr",
-  },
-];
+// const tops = [
+//   {
+//     brand: "Pull and bear",
+//     sizes: ["s", "m", "l", "xl"],
+//     color: "pink",
+//     price: "350kr.",
+//   },
+//   {
+//     brand: "Next",
+//     sizes: ["s", "m", "l"],
+//     color: "black",
+//     price: "325kr",
+//   },
+//   {
+//     brand: "Pull and bear",
+//     sizes: ["m", "l", "xl"],
+//     color: "brown",
+//     price: "400kr",
+//   },
+// ];
+// const topslist = document.querySelector(".topslist");
+// tops.forEach(showTops);
 
-// console.log(tops[2].color);
-const topslist = document.querySelector("#topslist");
+// function showTops(top) {
+//   topslist.innerHTML += `
+//   <article class="card">
+//   <h3>${top.brand}</3>
+//   <h2>${top.sizes}</h2>
+//   <p>${top.color}</p>
+//   <p>${top.price}</p>
+//   </article>
+//   `;
+// }
 
-function showTops(tops) {
-  console.log(tops);
-  let markup = "";
-  tops.forEach(
-    (top) =>
-      (markup += `
-  <article class="card">
-  <h3>${top.brand}</h3>
-  <h2>${top.price}</h2>
-  <p>${top.sizes}</p>
-  <p>${top.color}</p>
-</article>
-  `),
-  );
-  topslist.innerHTML = markup;
+const endpoint = `https://kea-alt-del.dk/t7/api/products?limit=20`;
+
+const topslist = document.querySelector(".topslist");
+
+fetch(endpoint).then((res) => res.json().then(visData));
+
+function visData(json) {
+  json.forEach((element) => {
+    topslist.innerHTML += `
+   <article class="card">
+   <h2>${element.productdisplayname}</2>
+   <h3>${element.brandname}</3>
+   <p>${element.price}</p>
+   </article>
+ `;
+  });
 }
