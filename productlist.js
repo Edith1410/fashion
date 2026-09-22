deb09c1843c547cd176eaa5d396eac5619405c2d
Edith Obdrup <edob1000@stud.ek.dk>
2026-09-22 14:36:12 +0200

    tilføjede billeder

--- productlist.js
+++ productlist.js
@@ -32,7 +32,7 @@
 //   `;
 // }
 
-const endpoint = `https://kea-alt-del.dk/t7/api/products?limit=20`;
+const endpoint = `https://kea-alt-del.dk/t7/api/products?limit=21`;
 
 const topslist = document.querySelector(".topslist");
 
@@ -42,10 +42,12 @@ function visData(json) {
   json.forEach((element) => {
     topslist.innerHTML += `
    <article class="card">
-   <img> ${element.picture}</img>
+   <img src=${`https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp`} alt="produktbillede"/>
    <h2>${element.productdisplayname}</2>
    <h3>${element.brandname}</3>
    <p>${element.price}</p>
+   <p>${element.gender}</p>
+   <p>${element.category}</p>
    </article>
  `;
   });
