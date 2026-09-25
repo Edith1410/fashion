d63219cd63dc89c34ba13ce3a3e57ddf5aa6997e
Edith Obdrup <edob1000@stud.ek.dk>
2026-09-25 10:01:27 +0200

    forside

--- productlist.js
+++ productlist.js
@@ -1,40 +1,9 @@
-// const tops = [
-//   {
-//     brand: "Pull and bear",
-//     sizes: ["s", "m", "l", "xl"],
-//     color: "pink",
-//     price: "350kr.",
-//   },
-//   {
-//     brand: "Next",
-//     sizes: ["s", "m", "l"],
-//     color: "black",
-//     price: "325kr",
-//   },
-//   {
-//     brand: "Pull and bear",
-//     sizes: ["m", "l", "xl"],
-//     color: "brown",
-//     price: "400kr",
-//   },
-// ];
-// const topslist = document.querySelector(".topslist");
-// tops.forEach(showTops);
-
-// function showTops(top) {
-//   topslist.innerHTML += `
-//   <article class="card">
-//   <h3>${top.brand}</3>
-//   <h2>${top.sizes}</h2>
-//   <p>${top.color}</p>
-//   <p>${top.price}</p>
-//   </article>
-//   `;
-// }
-
 const endpoint = `https://kea-alt-del.dk/t7/api/products?limit=21`;
 
 const topslist = document.querySelector(".topslist");
+
+const productid = 123456;
+const imagePath = `https://kea-alt-del.dk/t7/images/webp/640/${productid}.webp`;
 
 fetch(endpoint).then((res) => res.json().then(visData));
 
